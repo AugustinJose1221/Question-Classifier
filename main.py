db386f0a88d372d2ab6fd03ecb64d7928dd92c2b
AugustinJose1221 <augustinjose1221@gmail.com>
2020-06-18 19:14:53 +0530

    Changes made

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ Created on Thu Jun 18 17:50:33 2020
 @author: augustinjose
 """
 
+with open("dataset.txt", "r+") as file:
+    text = file.readlines()
+who = []   
+what = []
+when = []
+affirmation = []
+unknown = []
+for i in range(len(text)):
+    line = text[i].split(",,,")
+    if line[1] ==" who\n":
+        who.append(line[0])
+    if line[1] ==" what\n":
+        what.append(line[0])
+    if line[1] ==" when\n":
+        when.append(line[0])
+    if line[1] ==" affirmation\n":
+        affirmation.append(line[0])
+    if line[1] ==" unknown\n":
+        unknown.append(line[0])
 
 from eywa.nlu import Classifier
 
-
-x_hotel = ['book a hotel', 'need a nice place to stay', 'any motels near by']
-x_weather = ['what is the weather like', 'is it hot outside']
-
 clf = Classifier()
-clf.fit(x_hotel, 'hotel')
-clf.fit(x_weather, 'weather')
-
-print(clf.predict('will it rain today'))  # >>> 'weather'
-print(clf.predict('find a place to stay'))  # >>> 'hotel'
+clf.fit(who, 'who')
+clf.fit(what, 'what')
+clf.fit(when, 'when')
+clf.fit(affirmation, 'affirmation')
+clf.fit(unknown, 'unknown')
+x = input("Enter string :")
+print(clf.predict(str(x)))  
